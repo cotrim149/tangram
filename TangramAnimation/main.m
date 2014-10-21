9e9aa078ee607d030ddbdbcd4efe2798a06a9ee8
Victor Cotrim <cotrim149@gmail.com>
2014-10-21 14:40:36 -0200

    Initial Commit

---
+++ TangramAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TangramAnimation
+//
+//  Created by Victor de Lima on 21/10/14.
+//  Copyright (c) 2014 Victor de Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
