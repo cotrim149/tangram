93fc66479a7e1b685c7aebc38241e0f87f45a032
Victor Cotrim <cotrim149@gmail.com>
2014-10-22 15:50:31 -0200

    solved position of triangle 6

--- TangramAnimation/ViewController.m
+++ TangramAnimation/ViewController.m
@@ -57,7 +57,7 @@
     
 
     //Triangulo 7
-    CGRect rectTriangulo7 = CGRectMake(240+rect2.size.width, 133+rect2.size.height, 50, 100);
+    CGRect rectTriangulo7 = CGRectMake(241+rect2.size.width, 132+rect2.size.height, 50, 100);
     Triangle *triangle7 = [[Triangle alloc] initWithFrame:rectTriangulo7];
     triangle7.red = 20;
     triangle7.green = 40;
@@ -90,7 +90,7 @@
     
     
     //triangulo 6
-    CGRect rectTrianguloMenor6 = CGRectMake(291, 56, 50, 100);
+    CGRect rectTrianguloMenor6 = CGRectMake(292, 57, 50, 100);
     Triangle* trianguloMenor6 = [[Triangle alloc]initWithFrame:rectTrianguloMenor6];
     trianguloMenor6.red = 100;
     trianguloMenor6.green = 200;
@@ -98,32 +98,6 @@
     trianguloMenor6.backgroundColor = [UIColor clearColor];
     [self.view addSubview:trianguloMenor6];
     trianguloMenor6.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(1.4, 1.4),CGAffineTransformMakeRotation(180*M_PI/180));
-
-//
-//    //triangulo 1
-//    CGRect rectTriangulo1 = CGRectMake(120, 120, 50, 100);
-//    Triangle* triangulo1 = [[Triangle alloc]initWithFrame:rectTriangulo1];
-//    triangulo1.red = 0;
-//    triangulo1.green = 0;
-//    triangulo1.blue = 225;
-//    triangulo1.backgroundColor = [UIColor clearColor];
-//    [self.view addSubview:triangulo1];
-//    triangulo1.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(3,3),CGAffineTransformMakeRotation(M_PI/180));
-//    
-//
-//    
-//    //triangulo 2
-//    CGRect rectTrianguloMaior2 = CGRectMake(185, 55, 50, 100);
-//    Triangle* trianguloMaior2 = [[Triangle alloc]initWithFrame:rectTrianguloMaior2];
-//    trianguloMaior2.red = 80;
-//    trianguloMaior2.green = 200;
-//    trianguloMaior2.blue = 100;
-//    trianguloMaior2.backgroundColor = [UIColor clearColor];
-//    [self.view addSubview:trianguloMaior2];
-//    trianguloMaior2.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(3, 3),CGAffineTransformMakeRotation(90*M_PI/180));
-//    
-    
-    
 
     
 }
